8744ab4092a4797ec542547505c1a6ba7c83114a
Sergey Vengilevskiy <NeoVic2006@gmail.com>
2020-12-23 15:30:24 -0600

    Migrating Heroku

--- Craiglist/Craiglist/settings.py
+++ Craiglist/Craiglist/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -124,3 +125,5 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),) 
+
+django_heroku.settings(locals())
